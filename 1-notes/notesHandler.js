4e73381618dbeeeab40ebcb386cf8f750524d54a
Nicolás Omar González Passerino <nicolas.passerino@gmail.com>
2020-02-23 19:42:58 -0300

    note-add script fixed. Duplicated notes logic added

--- 1-notes/notesHandler.js
+++ 1-notes/notesHandler.js
@@ -5,9 +5,15 @@ const getNotes = () => console.log('test')
 
 const addNote = (newNote) => {
     const notes = loadNotes()
-    notes.push(newNote)
-    saveNotes(notes)
-    msg.success('New note added')
+    const hasDuplicated = notes.find((item) => item.title === newNote.title)
+
+    if(hasDuplicated) {
+        msg.fail('There is an already created note.')
+    } else {
+        notes.push(newNote)
+        saveNotes(notes)
+        msg.success('New note added.')
+    }
 }
 
 const saveNotes = (notesArray) => fs.writeFileSync('note.json', JSON.stringify(notesArray))
